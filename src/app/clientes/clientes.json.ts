140a7b7a133e15b2980bc00c7120ef38f06cd980
macs2008 <71674625+macs2008@users.noreply.github.com>
2022-02-14 16:48:45 -0500

    prueba
    prueba

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,15 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  { id: 1, nombre: 'Andrés', apellido: 'Guzmán', email: 'profesor@bolsadeideas.com', phone:1234,createAt: '2017-12-11',key:"prueba" },
+  { id: 2, nombre: 'Mr. John', apellido: 'Doe', email: 'john.doe@gmail.com', phone:1234,createAt: '2017-11-11',key:"prueba"  },
+  { id: 3, nombre: 'Linus', apellido: 'Torvalds', email: 'linus.torvalds@gmail.com',phone:1234, createAt: '2017-11-12',key:"prueba"  },
+  { id: 4, nombre: 'Rasmus', apellido: 'Lerdorf', email: 'rasmus.lerdorf@gmail.com',phone:1234, createAt: '2017-11-13',key:"prueba"  },
+  { id: 5, nombre: 'Erich', apellido: 'Gamma', email: 'erich.gamma@gmail.com',phone:1234, createAt: '2017-11-14',key:"prueba"  },
+  { id: 6, nombre: 'Richard', apellido: 'Helm', email: 'richard.helm@gmail.com',phone:1234, createAt: '2017-11-15',key:"prueba"  },
+  { id: 7, nombre: 'Ralph', apellido: 'Johnson', email: 'ralph.johnson@gmail.com',phone:1234, createAt: '2017-11-16',key:"prueba"  },
+  { id: 8, nombre: 'Bombasto', apellido: 'Vlissides', email: 'bombasto.vlissides@gmail.com',phone:1234, createAt: '2017-11-17',key:"prueba"  },
+  { id: 9, nombre: 'Dr James', apellido: 'Gosling', email: 'james.gosling@gmail.com',phone:1234, createAt: '2017-11-18',key:"prueba"  },
+  { id: 10, nombre: 'Magma', apellido: 'Lee', email: 'magma.lee@gmail.com',phone:1234, createAt: '2017-11-19',key:"prueba"  },
+  { id: 11, nombre: 'Tornado', apellido: 'Roe', email: 'tornado.roe@gmail.com',phone:1234, createAt: '2017-11-20',key:"prueba"  }
+];
